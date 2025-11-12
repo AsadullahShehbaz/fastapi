a402a7af433adc5825e9fe46c184053ea5f5d1ad
Asadullah Shehbaz <asadullahcreative@gmail.com>
2025-11-12 02:54:12 +0000

    file updated

--- codes/6-sqlalchemy-setup.py
+++ codes/6-sqlalchemy-setup.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import declarative_base , sessionmaker,Session
 DB_URL = 'sqlite:///./students.db'
 
 engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
@@ -16,7 +16,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer,primary_key=True,index=True)
     name = Column(String,nullable=False)
-    email = Column(String,unique=True,nullable=False)
+    email = Column(String,unique=True,index=True,nullable=False)
 Base.metadata.create_all(bind=engine)
 
 class UserCreate(BaseModel):
@@ -30,3 +30,37 @@ class UserResponse(BaseModel):
       class Config:
             orm_mode = True
 
+class UserUpdate(BaseModel):
+      name : Optional[str] = None
+      email : Optional[EmailStr] = None 
+
+app = FastAPI()
+
+def get_db():
+      db = SessionLocal()
+      try:
+            yield db 
+      finally:
+            db.close()
+@app.get('/')
+def welcome():
+      return {
+            "Name":"Ayan Ahmed",
+            "Level":"6"
+      }
+
+
+@app.post("/create",response_model=UserResponse)
+def create_user(user:UserCreate,db:Session = Depends(get_db)):
+      if db.query(User).filter(User.email == user.email).first():
+            raise HTTPException(status_code=400,detail='Email already exists')
+      db_user = User(name=user.name,email=user.email)
+      db.add(db_user)
+      db.commit()
+      db.refresh(db_user)
+      return db_user
+
+
+@app.get("/read",response_model=UserResponse)
+def read_user(db:Session = Depends(get_db)):
+      return db.query(User).all()
